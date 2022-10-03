eb506d1eafdde9317100efc165f7a8da29f7d615
Michael McCarthy <02michael.mccarthy@gulllakecs.org>
2022-10-03 08:07:37 -0400

    Add Print Statements to Welcome Visitors

--- OperationFury.py
+++ OperationFury.py
@@ -3,5 +3,10 @@
 #Version: 1.0
 
 """
-Our welcome Screen will start our program letting drivers know that the InfoTechCenter OS is loading
+Our welcome Screen will start our program letting
+drivers know that the InfoTechCenter OS is loading
 """
+
+print("\n\nWelcome to Operation Fury InfoTech Center\n")
+
+print("\nOperation Fury's Operation System Booting Up")
