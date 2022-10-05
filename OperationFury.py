33da51eee97fc30ecc32823bbcbdbc2300a1f605
Michael McCarthy <02michael.mccarthy@gulllakecs.org>
2022-10-05 08:38:51 -0400

    Initial Weather Commit

---
+++ OperationFury.py
@@ -0,0 +1,9 @@
+#weather
+#developer: Michael McCarthy
+#Version 1.o
+"""
+Create a function for our weather using the
+random.choice function to determine what the weather is
+picking from a list - using if, elif, and else statements
+to check the condition and print a specific print line
+"""
