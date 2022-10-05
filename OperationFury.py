25108a26163ee0cf213e21881619c3c711753439
Michael McCarthy <02michael.mccarthy@gulllakecs.org>
2022-10-05 08:06:23 -0400

    Add for loop "OS Booting Up"

--- OperationFury.py
+++ OperationFury.py
@@ -8,8 +8,11 @@ drivers know that the InfoTechCenter OS is loading
 """
 
 #Import Libraries Here
-from time import sleep #We imported the Sleep function from the Time Librart
+from time import sleep #We imported the Sleep function from the Time Library
 
 print("\n\nWelcome to Operation Fury InfoTech Center\n")
 sleep(1.5)
-print("\033[1;28 +\nOperation Fury's Operation System Booting Up")
+print("\033[1;36m  \nOperation Fury's Operation System Booting Up")
+for i in range(2):
+    print("OS Booting up")
+    sleep(.5)
