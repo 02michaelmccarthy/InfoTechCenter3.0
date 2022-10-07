b9d05655035713618e39cb431905c3ad4ec8506a
Michael McCarthy <02michael.mccarthy@gulllakecs.org>
2022-10-07 08:15:22 -0400

    Create my vehicle Response System Function

--- OperationFury.py
+++ OperationFury.py
@@ -11,10 +11,19 @@ to check the condition and print a specific print line
 
 #Import Libraries here
 import random
-#Weather condition list using the randome.choice library
-#to randomly chose a codnition and storing it in its brain
+#Weather condition list using the random.choice library
+#to randomly chose a condition and storing it in its brain
 def weather():
     weatherForecast = ["Rain","Snow","Sunny","Cloudy","Foggy","Storming","Icy"]
     weatherCondition = random.choice(weatherForecast)
     return weatherCondition
 
+
+weatherAlert = weather()
+def vRS():
+    if weatherAlert == "Icy":
+        print("\n VRS has changed your alarm 30 minutes earlier based on the NWS forcast of" ,weatherAlert)
+
+
+
+vRS()
