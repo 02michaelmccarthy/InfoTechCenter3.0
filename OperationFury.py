34415d89337cf4729f0de603b3b3d5612a50b386
Michael McCarthy <02michael.mccarthy@gulllakecs.org>
2022-10-10 07:58:46 -0400

    Added more elif and Else statements checking weather conditions

--- OperationFury.py
+++ OperationFury.py
@@ -14,7 +14,7 @@ import random
 #Weather condition list using the random.choice library
 #to randomly chose a condition and storing it in its brain
 def weather():
-    weatherForecast = ["Rain","Snow","Sunny","Cloudy","Foggy","Storming","Icy"]
+    weatherForecast = ["Rain","Snow","Sunny","Windy","Foggy","Storming","Icy"]
     weatherCondition = random.choice(weatherForecast)
     return weatherCondition
 
@@ -23,7 +23,25 @@ weatherAlert = weather()
 def vRS():
     if weatherAlert == "Icy":
         print("\n VRS has changed your alarm 30 minutes earlier based on the NWS forcast of" ,weatherAlert)
-
+        print("VRS will only allow your car to go 30MPH")
+    elif weatherAlert == "Snow":
+        print("\n VRS has changed your alarm 15 minutes earlier based on the NWS forcast of", weatherAlert)
+        print("VRS will only allow your car to go 50MPH")
+    elif weatherAlert == "Rain":
+        print("\n VRS has changed your alarm 5 minutes earlier based on the NWS forcast of", weatherAlert)
+        print("VRS will only allow your car to go 60MPH")
+    elif weatherAlert == "Windy":
+        print("\n VRS has changed your alarm 5 minutes earlier based on the NWS forcast of", weatherAlert)
+        print("VRS will only allow your car to go 70MPH")
+    elif weatherAlert == "Foggy":
+        print("\n VRS has changed your alarm 5 minutes earlier based on the NWS forcast of", weatherAlert)
+        print("VRS will only allow your car to go 60MPH")
+    elif weatherAlert == "Storming":
+        print("\n VRS has changed your alarm 5 minutes earlier based on the NWS forcast of", weatherAlert)
+        print("VRS will only allow your car to go 60MPH")
+    else:
+        print("\n The weather today is", weatherAlert, ",let's Go!")
+        print("VRS will only allow your car to go 90MPH")
 
 
 vRS()
