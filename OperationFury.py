3056d7c4fc6937823a9fa6878e120b24f4cfa497
Michael McCarthy <02michael.mccarthy@gulllakecs.org>
2022-10-10 08:40:03 -0400

    Defined the Gas Level Gauge Function

---
+++ OperationFury.py
@@ -0,0 +1,18 @@
+#Gasoline
+#Programmer: Michael McCarthy
+#Version 1.0
+
+"""
+Define a function to check our gas gauge and determine how far
+we have until we need gasoline based on an if, elif, else
+condition
+"""
+
+#import library here
+import random
+
+#Gas Level Function
+def gasLevelGauge():
+    gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarter Tank", "Full Tank"]
+    currentGasLevel = random.choice(gasLevelList)
+    return currentGasLevel
