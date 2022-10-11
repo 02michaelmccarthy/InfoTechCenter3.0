6a0d1a08ede610f7a27d1cae9c73917523daa66f
Michael McCarthy <02michael.mccarthy@gulllakecs.org>
2022-10-11 08:40:51 -0400

    Defined listOfGasStations function

--- OperationFury.py
+++ OperationFury.py
@@ -12,6 +12,8 @@ condition
 import random
 from time import sleep
 
+number = random.randint(1,20)
+
 #Gas Level Function
 def gasLevelGauge():
     gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank", "Three Quarter Tank", "Full Tank"]
@@ -21,10 +23,22 @@ def gasLevelGauge():
 #Variable calling the gasLevelGuage function to store value once
 gasLevelIndicator = gasLevelGauge()
 
+def listOfGasStations():
+    gasStation = ["Shell", "Circle K", "Marathon", "Speedway", "Meijer"]
+    GasStationNearby = random.choice(gasStation)
+    return GasStationNearby
 def gasLevelAlert():
     if gasLevelIndicator == "Empty":
         print("***WARNING YOU ARE ON EMPTY***")
         sleep(1)
         print("Calling Your Emergency Contact")
+    elif gasLevelIndicator == "Low":
+        print("***WARNING***")
+        sleep(1)
+        print("You are running extremely low on gas")
+        sleep(0.5)
+        print("Checking google maps for nearest gas station")
+        sleep(0.5)
+        print("The nearest gas station is", listOfGasStations(), ". ", listOfGasStations(), " is ", number, " miles away")
 
 gasLevelAlert()
